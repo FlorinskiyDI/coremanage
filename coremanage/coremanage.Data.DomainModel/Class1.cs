2d44cd52bea96791d4c6ed333659ec0ab9ed05e2
FlorinskiyDI <dmytro.florynskyi@gmail.com>
2017-03-02 18:59:01 +0200

    added entity - Audited

--- coremanage/coremanage.Data.DomainModel/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace coremanage.Data.DomainModel
-{
-    public class Class1
-    {
-    }
-}
---
+++ coremanage/coremanage.Data.Storage.EFCore.Common/DbContexts/DashboardDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coremanage.Data.Storage.EFCore.Common.DbContexts
+{
+    class DashboardDbContext
+    {
+    }
+}
