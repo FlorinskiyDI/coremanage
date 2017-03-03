837468e36a716cfeb6e6ba9d6f4f9ada30e16ce5
FlorinskiyDI <dmytro.florynskyi@gmail.com>
2017-03-03 17:39:19 +0200

    added AddCoreStorageEFCoreMSSQL

--- coremanage/coremanage.Data.Storage.EFCore.MSSQL/DashboardBuilderExtensions.cs
+++ coremanage/coremanage.Data.Storage.EFCore.MSSQL/DashboardBuilderExtensions.cs
@@ -7,4 +7,23 @@ namespace coremanage.Data.Storage.EFCore.MSSQL
     class DashboardBuilderExtensions
     {
     }
+    public static IServiceCollection AddCoreStorageEFCoreMSSQL(
+            this IServiceCollection services,
+            string connectionString
+        )
+    {
+        // Add framework services.
+        services.AddDbContext<IdentityServerDbContext>(options => options.UseSqlServer(connectionString, b => b.MigrationsAssembly("coremanage.IdentityServer.Storage.EFCore.MSSQL")));
+        services.AddIdentity<AppUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 6;
+        })
+            .AddEntityFrameworkStores<IdentityServerDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
 }
