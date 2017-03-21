adf9121f68fc542f22199c19c88d8e5bcd4e5c9e
FlorinskiyDI <dmytro.florynskyi@gmail.com>
2017-03-21 16:54:25 +0200

    added ServiceCollectionExtentions

---
+++ coremanage/storagecore.EntityFrameworkCore/Startup/ServiceCollectionExtentions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using storagecore.Abstractions.Context;
+using storagecore.Abstractions.Repositories;
+using storagecore.Abstractions.Uow;
+using storagecore.EntityFrameworkCore.Context;
+using storagecore.EntityFrameworkCore.Repositories;
+using storagecore.EntityFrameworkCore.Uow;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace storagecore.EntityFrameworkCore.Startup
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static IServiceCollection AddDataAccess<TEntityContext>(this IServiceCollection services) where TEntityContext : DbContextBase<TEntityContext>
+        {
+            RegisterDataAccess<TEntityContext>(services);
+            return services;
+        }
+
+        private static void RegisterDataAccess<TEntityContext>(IServiceCollection services) where TEntityContext : DbContextBase<TEntityContext>
+        {
+            services.TryAddSingleton<IUowProvider, UowProvider>();
+            services.TryAddTransient<IEntityContext, TEntityContext>();
+            services.TryAddTransient(typeof(IBaseRepository<,>), typeof(GenericRepository<,>));
+            //services.TryAddTransient(typeof(IDataPager<>), typeof(DataPager<>));
+        }
+    }
+}
