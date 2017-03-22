26ef8950969b24b359cbee81be25f9aa2ce75021
FlorinskiyDI <dmytro.florynskyi@gmail.com>
2017-03-22 14:59:26 +0200

    added TenantController

--- coremanage/coremanage.Dashboard.WebApi/Controllers/TenantController.cs
+++ coremanage/coremanage.Dashboard.WebApi/Controllers/TenantController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using coremanage.Core.Services.Shared.API.Security;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace coremanage.Dashboard.WebApi.Controllers
 {
     [Route("api/Tenant")]
-    public class TenantController : Controller
+    public class TenantController: Controller
     {
+        private readonly ICompanyService _companyService;
+        public TenantController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+
         // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
