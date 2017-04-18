c0f415ccbbc9c877cf52e5b2c1f1b59d8b53c6c7
FlorinskiyDI <dmytro.florynskyi@gmail.com>
2017-04-18 13:05:55 +0300

    added Observable to refresh-select-tenant.component

--- coremanage/coremanage.Dashboard.Web/src/app/shared/components/refresh-select-tenant/refresh-select-tenant.component.ts
+++ coremanage/coremanage.Dashboard.Web/src/app/shared/components/refresh-select-tenant/refresh-select-tenant.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgRedux, select } from '@angular-redux/store';
+import { IAppState } from '../../../redux/store';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'refresh-select-tenant-component',
@@ -7,5 +10,27 @@ import { Router } from '@angular/router';
     styleUrls: ['./refresh-select-tenant.component.scss'],
 })
 
-export class RefreshSelectTenantComponent {   
+export class RefreshSelectTenantComponent {    
+    // private tenantList$: Observable<string[]>;
+    private appState: IAppState;
+
+
+    @select(['session', 'user', 'tenant_list']) tenantList$: Observable<any>;
+    constructor(       
+        private ngRedux: NgRedux<IAppState>
+    ){
+        // this.tenantList$ = this.ngRedux.select('tenant_list');
+    }
+
+    ngOnInit() {
+        this.tenantList$.subscribe((value: any) => {
+            if (value) {
+                console.log(value);
+            }
+        });
+    }
+    // 
+    haveSubTopics(event: any): void {
+        
+    }
 }
